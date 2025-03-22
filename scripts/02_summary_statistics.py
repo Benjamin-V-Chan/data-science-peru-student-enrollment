d04cb301b773fdfd73cac0817ba93c0b6f4a24ee
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-21 23:35:53 -0700

    02_summary_statistics script outline

---
+++ scripts/02_summary_statistics.py
@@ -0,0 +1,8 @@
+# Load cleaned data
+# Print and save:
+# - Number of students by enrollment type
+# - Tuition payment rates
+# - Gender distribution
+# - Distribution of study modes and shifts
+# - Courses enrolled vs. at-risk
+# Save results to a text file in outputs/
