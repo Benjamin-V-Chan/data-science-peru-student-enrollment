a197526f1c99295f4a0a849d736853884c82ffca
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-21 23:36:12 -0700

    03_visualizations script outline

---
+++ scripts/03_visualizations.py
@@ -0,0 +1,7 @@
+# Load cleaned data
+# Plot and save:
+# - Bar chart of enrollment types
+# - Pie chart of gender
+# - Histogram of courses enrolled
+# - Heatmap of tuition payment 2022 vs 2023
+# Save all visualizations to outputs/visualizations/
